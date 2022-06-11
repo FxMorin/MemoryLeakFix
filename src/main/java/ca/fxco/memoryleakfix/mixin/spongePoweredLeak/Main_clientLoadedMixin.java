04788e5ab3b4e49054768c1985cc7690dc399dcc
FX - PR0CESS <fx.e.morin@gmail.com>
2022-06-11 11:02:03 -0400

    Better injection for client forceload call

--- src/main/java/ca/fxco/memoryleakfix/mixin/spongePoweredLeak/Main_clientLoadedMixin.java
+++ src/main/java/ca/fxco/memoryleakfix/mixin/spongePoweredLeak/Main_clientLoadedMixin.java
@@ -3,26 +3,26 @@ package ca.fxco.memoryleakfix.mixin.spongePoweredLeak;
 import ca.fxco.memoryleakfix.MemoryLeakFix;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.main.Main;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
 @Mixin(Main.class)
 public class Main_clientLoadedMixin {
 
 
-    @Redirect(
+    @Inject(
             method = "main([Ljava/lang/String;)V",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/MinecraftClient;shouldRenderAsync()Z"
+                    target = "Lnet/minecraft/client/MinecraftClient;shouldRenderAsync()Z",
+                    shift = At.Shift.BEFORE
             )
     )
-    private static boolean loadAllMixinsThenShouldRenderAsync(MinecraftClient instance) {
+    private static void loadAllMixinsThenShouldRenderAsync(String[] args, CallbackInfo ci) {
         MemoryLeakFix.forceLoadAllMixinsAndClearSpongePoweredCache();
-        return instance.shouldRenderAsync();
     }
 }
