e89b3068163666aff063e76d3ca8cb572e2a4de2
FX - PR0CESS <28154542+fmorin@users.noreply.github.com>
2022-07-19 13:38:44 -0400

    Fix remap warning message

--- src/main/java/ca/fxco/memoryleakfix/mixin/accessor/ByteBufAccessor.java
+++ src/main/java/ca/fxco/memoryleakfix/mixin/accessor/ByteBufAccessor.java
@@ -4,7 +4,7 @@ import io.netty.buffer.ByteBuf;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
-@Mixin(ByteBuf.class)
+@Mixin(value = ByteBuf.class, remap = false)
 public interface ByteBufAccessor {
     @Invoker("isAccessible")
     boolean invokeIsAccessible();
