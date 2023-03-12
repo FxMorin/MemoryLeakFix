04960f79420c6fec83ba6fe17faeaa2cb9da1a7e
contaria <kingcontaria@gmail.com>
2023-03-12 18:00:03 +0100

    fix Entity_clearMemoriesMixin on forge

--- common/src/main/java/ca/fxco/memoryleakfix/mixin/entityMemoriesLeak/Entity_clearMemoriesMixin.java
+++ common/src/main/java/ca/fxco/memoryleakfix/mixin/entityMemoriesLeak/Entity_clearMemoriesMixin.java
@@ -13,15 +13,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Entity.class)
 public abstract class Entity_clearMemoriesMixin {
 
-    // method_5650 is the intermediary name which is needed because in older Minecraft versions the parameters of the method were different
     @Group(name = "memoryLeakFix$onEntityRemoved", min = 1, max = 1)
     @Inject(method = "remove", at = @At("TAIL"))
     protected void memoryLeakFix$OnEntityRemoved(CallbackInfo ci) {
     }
 
+    // method_5650 (Fabric) and m_142687_ (Forge) are the intermediary names which are needed because in older Minecraft versions the parameters of the method were different
     @Group(name = "memoryLeakFix$onEntityRemoved", min = 1, max = 1)
     @SuppressWarnings({"UnresolvedMixinReference", "MixinAnnotationTarget"})
-    @Inject(method = "method_5650", at = @At("TAIL"), remap = false)
+    @Inject(method = {"method_5650", "m_142687_"}, at = @At("TAIL"), remap = false)
     protected void memoryLeakFix$OnEntityRemoved_inOlderVersions(CallbackInfo ci) {
         this.memoryLeakFix$OnEntityRemoved(ci);
     }
