38f6e2daaea3f3a7b8ec9a1c762ed76831deb7fc
fxmorin <28154542+fxmorin@users.noreply.github.com>
2023-03-11 20:59:07 -0500

    suppress warning

--- common/src/main/java/ca/fxco/memoryleakfix/mixin/entityMemoriesLeak/Entity_clearMemoriesMixin.java
+++ common/src/main/java/ca/fxco/memoryleakfix/mixin/entityMemoriesLeak/Entity_clearMemoriesMixin.java
@@ -13,6 +13,7 @@ public abstract class Entity_clearMemoriesMixin {
 
     // method_5650 is the intermediary name which is needed because in older Minecraft versions the parameters of the method were different
     // we require 0 because using the intermediary name will probably cause issues on older minecraft versions with forge, and we simply don't care enough atm ¯\_(ツ)_/¯
+    @SuppressWarnings("UnresolvedMixinReference")
     @Inject(method = {"remove", "method_5650"}, at = @At("TAIL"), require = 0, allow = 1)
     protected void memoryLeakFix$OnEntityRemoved(CallbackInfo ci) {
     }
