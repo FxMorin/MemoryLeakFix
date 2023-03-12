ff2b827abdb090dd1f60d300ab892147bc818d84
contaria <kingcontaria@gmail.com>
2023-03-12 05:36:19 +0100

    java 8 compatibility for forge

--- forge/src/main/java/ca/fxco/memoryleakfix/forge/MemoryLeakFixExpectPlatformImpl.java
+++ forge/src/main/java/ca/fxco/memoryleakfix/forge/MemoryLeakFixExpectPlatformImpl.java
@@ -3,10 +3,30 @@ package ca.fxco.memoryleakfix.forge;
 import ca.fxco.memoryleakfix.MemoryLeakFixExpectPlatform;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLLoader;
+import org.apache.maven.artifact.versioning.ArtifactVersion;
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 
 @SuppressWarnings("unused")
 public class MemoryLeakFixExpectPlatformImpl {
+
+    private static final ArtifactVersion MCVERSION;
+
+    static {
+        String mcVersion;
+        try {
+            // new forge
+            Object versionInfo = FMLLoader.class.getMethod("versionInfo").invoke(null);
+            mcVersion = (String) versionInfo.getClass().getMethod("mcVersion").invoke(versionInfo);
+        } catch (Exception e) {
+            try {
+                // old forge
+                mcVersion = (String) FMLLoader.class.getField("mcVersion").get(null);
+            } catch (Exception ex) {
+                throw new RuntimeException("[MemoryLeakFix] Reflection failed at getting the Minecraft version from Forge", ex);
+            }
+        }
+        MCVERSION = new DefaultArtifactVersion(mcVersion);
+    }
 
     /**
      * This is our actual method to {@link MemoryLeakFixExpectPlatform#isModLoaded}.
@@ -19,6 +39,6 @@ public class MemoryLeakFixExpectPlatformImpl {
      * This is our actual method to {@link MemoryLeakFixExpectPlatform#compareMinecraftToVersion}.
      */
     public static int compareMinecraftToVersion(String version) {
-        return new DefaultArtifactVersion(FMLLoader.versionInfo().mcVersion()).compareTo(new DefaultArtifactVersion(version));
+        return MCVERSION.compareTo(new DefaultArtifactVersion(version));
     }
 }
