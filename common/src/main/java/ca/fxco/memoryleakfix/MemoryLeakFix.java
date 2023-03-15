35138d5141abdf4c343aee0fd7a907e566986506
contaria <kingcontaria@gmail.com>
2023-03-15 02:41:21 +0100

    allow compilation for 1.16.5 forge

--- common/src/main/java/ca/fxco/memoryleakfix/MemoryLeakFix.java
+++ common/src/main/java/ca/fxco/memoryleakfix/MemoryLeakFix.java
@@ -2,14 +2,6 @@ package ca.fxco.memoryleakfix;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.spongepowered.asm.logging.ILogger;
-import org.spongepowered.asm.logging.LoggerAdapterDefault;
-import org.spongepowered.asm.mixin.MixinEnvironment;
-import org.spongepowered.asm.mixin.injection.struct.InjectorGroupInfo;
-import org.spongepowered.asm.mixin.transformer.ClassInfo;
-
-import java.lang.reflect.Field;
-import java.util.*;
 
 public class MemoryLeakFix {
 
