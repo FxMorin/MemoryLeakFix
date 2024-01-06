b3dc3f160e012856171bdf6f38ccc4872628cc35
fxmorin <28154542+fxmorin@users.noreply.github.com>
2024-01-06 10:13:59 -0500

    Remove Packet memory leak since its causing issues and I don't have the time to fix it.

--- fabric/src/main/java/ca/fxco/memoryleakfix/fabric/MemoryLeakFixFabric.java
+++ fabric/src/main/java/ca/fxco/memoryleakfix/fabric/MemoryLeakFixFabric.java
@@ -1,24 +1,9 @@
 package ca.fxco.memoryleakfix.fabric;
 
 import ca.fxco.memoryleakfix.MemoryLeakFix;
-import ca.fxco.memoryleakfix.MemoryLeakFixExpectPlatform;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.Version;
-import net.fabricmc.loader.api.VersionParsingException;
-import net.fabricmc.loader.impl.FabricLoaderImpl;
-import net.minecraft.network.FriendlyByteBuf;
-import org.spongepowered.asm.logging.ILogger;
-import org.spongepowered.asm.logging.LoggerAdapterDefault;
-import org.spongepowered.asm.mixin.MixinEnvironment;
-import org.spongepowered.asm.mixin.injection.struct.InjectorGroupInfo;
-import org.spongepowered.asm.mixin.transformer.ClassInfo;
-
-import java.lang.reflect.Field;
-import java.util.*;
 
 public class MemoryLeakFixFabric implements ModInitializer {
-
-    public static final Set<FriendlyByteBuf> BUFFERS_TO_CLEAR = Collections.synchronizedSet(new HashSet<>());
 
     @Override
     public void onInitialize() {
